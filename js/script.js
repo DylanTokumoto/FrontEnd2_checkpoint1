9ef2fd56ed7b72953e002950f38ab72e923ac7b8
DylanTokumoto <heidy.tokumoto@gmail.com>
2022-05-27 17:40:05 -0300

    comecando o cleanCode

--- js/script.js
+++ js/script.js
@@ -1,18 +1,76 @@
-var url = document.getElementById('url')
-const image = document.getElementById('image-1')
-var submit = doocument.getElementById('form__button')
+const $title = document.getElementById('title')
+const $url = document.getElementById('url')
+const $description = document.getElementById('description')
+const $section = document.querySelector('.main__gallery')
+const $button = document.getElementById('form__button')
+const $buttonPush = [];
+
+
+function newCard() {
+
+    $button.addEventListener('click', function () {
+        $buttonPush.push('click');
+
+
+
+        // =========== CRIANDO O CARD ===========
+
+        let $card = document.createElement('div');
+        $card.setAttribute('class', 'gallery__card');
+        $section.insertAdjacentElement('beforeend', $card);
+
+
+
+        // =========== CRIANDO O TITULO ===========
+
+        let $div1 = document.createElement('div');
+        $div1.setAttribute('class', 'image__title');
+        $card.insertAdjacentElement('beforeend', $div1);
+
+
+        // =========== SETANDO O VALOR DE H1 ===========
+
+        let $titleCard = document.createElement('h1');
+        $div1.insertAdjacentElement('beforeend', $titleCard);
+        $titleCard.innerHTML = $title.value;
+
+
+
+        // =========== CRIANDO A TAG IMG ===========
+
+        let $div2 = document.createElement('div');
+        $div2.setAttribute('class', 'card__image');
+        $buttonPush.forEach(function (_, index) {
+            $div2.setAttribute('id', `image-${index + 1}`);
+        });
+        $card.insertAdjacentElement('beforeend', $div2);
 
 
 
 
+        // =========== SETANDO O VALOR DA IMG ===========
 
-function newImage() {
-    let image = document.getElementById('image-1');
-    let image1 = document.createElement('img');
-    image1.setAttribute('src', url.value )
-    image1.setAttribute('alt', 'Imagem adicionada pelo próprio usuário' )
+        let $image = document.createElement('img');
+        $image.setAttribute('src', $url.value);
+        $image.setAttribute('alt', 'Imagem adicionada pelo próprio usuário');
+        $div2.insertAdjacentElement('beforeend', $image);
 
-    image.insertAdjacentElement('beforeend', image)
+
+
+        // =========== CRIANDO A DESCRIÇÃO ===========
+
+        let $div3 = document.createElement('div');
+        $div3.setAttribute('class', 'image__description');
+        $card.insertAdjacentElement('beforeend', $div3);
+
+
+        // =========== SETANDO O VALOR DA DESCRIÇÃO ===========
+
+        let $imageDescription = document.createElement('h1');
+        $div3.insertAdjacentElement('beforeend', $imageDescription);
+        $imageDescription.innerHTML = $description.value;
+
+    });
 }
 
-newImage();
+newCard();
