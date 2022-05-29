9ad1130132366e6a06a35f0e005f40a39739c5fc
DylanTokumoto <heidy.tokumoto@gmail.com>
2022-05-29 12:05:19 -0300

    finalizando o script

--- js/script.js
+++ js/script.js
@@ -11,64 +11,30 @@ function newCard() {
     $button.addEventListener('click', function () {
         $buttonPush.push('click');
 
+        let arrayInfos = [{
+            titulo: $title.value,
+            descricao: $description.value,
+            image: $url.value
+        }];
+        
+        
 
-
-        // =========== CRIANDO O CARD ===========
-
-        let $card = document.createElement('div');
-        $card.setAttribute('class', 'gallery__card');
-        $section.insertAdjacentElement('beforeend', $card);
-
-
-
-        // =========== CRIANDO O TITULO ===========
-
-        let $div1 = document.createElement('div');
-        $div1.setAttribute('class', 'image__title');
-        $card.insertAdjacentElement('beforeend', $div1);
-
-
-        // =========== SETANDO O VALOR DE H1 ===========
-
-        let $titleCard = document.createElement('h1');
-        $div1.insertAdjacentElement('beforeend', $titleCard);
-        $titleCard.innerHTML = $title.value;
-
-
-
-        // =========== CRIANDO A TAG IMG ===========
-
-        let $div2 = document.createElement('div');
-        $div2.setAttribute('class', 'card__image');
-        $buttonPush.forEach(function (_, index) {
-            $div2.setAttribute('id', `image-${index + 1}`);
-        });
-        $card.insertAdjacentElement('beforeend', $div2);
-
-
-
-
-        // =========== SETANDO O VALOR DA IMG ===========
-
-        let $image = document.createElement('img');
-        $image.setAttribute('src', $url.value);
-        $image.setAttribute('alt', 'Imagem adicionada pelo próprio usuário');
-        $div2.insertAdjacentElement('beforeend', $image);
-
-
-
-        // =========== CRIANDO A DESCRIÇÃO ===========
-
-        let $div3 = document.createElement('div');
-        $div3.setAttribute('class', 'image__description');
-        $card.insertAdjacentElement('beforeend', $div3);
-
-
-        // =========== SETANDO O VALOR DA DESCRIÇÃO ===========
-
-        let $imageDescription = document.createElement('h1');
-        $div3.insertAdjacentElement('beforeend', $imageDescription);
-        $imageDescription.innerHTML = $description.value;
+        for (let i of arrayInfos) {
+        
+            var defaultCard = `<div class="gallery__card">
+            <h2>${i.titulo}</h2>
+            <img src="${i.image}"
+            <p>${i.descricao}</p>
+            </div>`
+        
+            var novoDefaultCard = document.createElement("div");
+        
+            novoDefaultCard.innerHTML = defaultCard;
+        
+            $section.appendChild(novoDefaultCard);
+        
+            
+        }
 
     });
 }
