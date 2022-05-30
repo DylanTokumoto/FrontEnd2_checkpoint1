c7eb01827ea6beb1a058c9345df62327086f1fcf
DylanTokumoto <heidy.tokumoto@gmail.com>
2022-05-30 16:07:11 -0300

    validando o form

--- js/script.js
+++ js/script.js
@@ -3,40 +3,76 @@ const $url = document.getElementById('url')
 const $description = document.getElementById('description')
 const $section = document.querySelector('.main__gallery')
 const $button = document.getElementById('form__button')
-const $buttonPush = [];
+const $form = document.getElementById('form')
+const formItem = document.querySelectorAll('.form__item')
+const error = document.querySelectorAll('.error')
+// const $buttonPush = [];
 
 
 function newCard() {
 
-    $button.addEventListener('click', function () {
-        $buttonPush.push('click');
+    let arrayInfos = [{
+        titulo: $title.value,
+        descricao: $description.value,
+        image: $url.value
+    }];
 
-        let arrayInfos = [{
-            titulo: $title.value,
-            descricao: $description.value,
-            image: $url.value
-        }];
-        
-        
+    for (let i of arrayInfos) {
 
-        for (let i of arrayInfos) {
-        
-            var defaultCard = `<div class="gallery__card">
+        var defaultCard = `
+            <div class="gallery__card">
             <h2>${i.titulo}</h2>
-            <img src="${i.image}"
+            <img src="${i.image}" alt="Imagem adicionada pelo próprio usuário!">
             <p>${i.descricao}</p>
-            </div>`
-        
-            var novoDefaultCard = document.createElement("div");
-        
-            novoDefaultCard.innerHTML = defaultCard;
-        
-            $section.appendChild(novoDefaultCard);
-        
-            
-        }
+            </div>
+        `
 
-    });
+        var novoDefaultCard = document.createElement("div");
+
+        novoDefaultCard.innerHTML = defaultCard;
+
+        $section.appendChild(novoDefaultCard);
+
+    }
+
+    // });
 }
 
-newCard();
+
+// ===================== validando o formulario =================
+
+$button.addEventListener('click', (e) => {
+
+    checkTitle();
+
+    function checkTitle() {
+        let mensagemErro = [];
+        if ($title.value === '' || $title.value == null) {
+            e.preventDefault();
+            mensagemErro.push('Você precisa adicionar um titulo');
+            error[0].innerText = mensagemErro;
+            formItem[0].classList.add('fail');
+        } else {
+            formItem[0].classList.remove('fail');
+            error[0].innerText = null;
+            formItem[0].classList.add('sucess');
+            checkDesc();
+        }
+    }
+
+    function checkDesc() {
+        let mensagemErro = [];
+        if ($description.value == null) {
+            console.log("teste")
+            e.preventDefault();
+            mensagemErro.push('Você precisa adicionar uma descrição com no máximo 120 caracteres 🥴🥴')
+            error[1].innerText = mensagemErro;
+            formItem[1].classList.add('fail');
+        } else {
+            formItem[1].classList.remove('fail');
+            error[1].innerText = null;
+            formItem[1].classList.add('sucess');
+            newCard();
+        }
+    }
+})
